faf0b618489802ece803ac6b40d709dffa7c7e92
Yehor Tuz <tuzegor@gmail.com>
2021-12-12 18:00:15 +0200

    LocalStorage done

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import ContactForm from './components/ContactForm';
+import Filter from './components/Filter';
+import ContactList from './components/ContactList';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contactsStorage'));
+    if (contacts !== null) {
+      this.setState({ contacts: contacts });
+    }
+  }
+  componentDidUpdate() {
+    localStorage.setItem(
+      'contactsStorage',
+      JSON.stringify(this.state.contacts),
+    );
+  }
+
+  getContactFromForm = contact => {
+    const { contacts } = this.state;
+    if (
+      contacts.find(
+        сontactItem =>
+          сontactItem.name.toLowerCase() === contact.name.toLowerCase(),
+      )
+    ) {
+      alert('Such contact exists');
+    } else {
+      this.setState({ contacts: [contact, ...contacts] });
+    }
+  };
+
+  findName = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  showFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+    return filteredContacts;
+  };
+
+  deleteContact = id => {
+    const { contacts } = this.state;
+
+    this.setState({ contacts: contacts.filter(obj => obj.id !== id) });
+  };
+
+  render() {
+    const { filter } = this.state;
+    return (
+      <div className="App">
+        <h1 className="title">Phonebook</h1>
+        <ContactForm submitContact={this.getContactFromForm} />
+        <h2 className="title">Contacts</h2>
+        <Filter filter={filter} findName={this.findName} />
+        <ContactList
+          showFilteredContacts={this.showFilteredContacts}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
